844547f2c36f773430b1e3db60df668b7ccc303d
Łukasz Osmólski <93lukaszo93@gmail.com>
2017-05-02 21:39:15 +0200

    2nd commit

--- scripts.js
+++ scripts.js
@@ -2,8 +2,7 @@ var list = document.getElementById ('list');
 var add = document.getElementById ('addElem');
 var number = document.getElementsByTagName ('li');
 
-console.log(number.length);
 
 add.addEventListener('click', function() {
-    list.innerHTML += '<li>item</li>';
+    list.innerHTML += '<li>item</li>' + number.length;
 });
